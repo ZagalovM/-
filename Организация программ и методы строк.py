99af1778fcc5e85debb3f56f6a7ff5a712a63d1f
ZagalovM <zagalov777@gmail.com>
2024-12-05 22:07:52 +0300

    Add files via upload

---
+++ Организация программ и методы строк.py
@@ -0,0 +1,11 @@
+#my_string = input()
+#print(my_string.upper())
+
+#my_string = input()
+#print(my_string.upper().lower())
+
+#my_string = input()
+#print(my_string.replace(' ', ''))
+
+my_string = input()
+print(my_string[-1])
